9dbae16faec5387fd4ee14f343db216fc7776adf
martincz <martincz@mokeedev.com>
2014-04-01 17:45:09 +0800

    mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,18 +1,18 @@
 $(call inherit-product, device/htc/m4/full_m4.mk)
 
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mk/config/gsm.mk)
 
 PRODUCT_RELEASE_NAME := m4
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/m4/device.mk)
 
 # Device naming
 PRODUCT_DEVICE := m4
-PRODUCT_NAME := cm_m4
+PRODUCT_NAME := mk_m4
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := One Mini
 PRODUCT_MANUFACTURER := HTC
